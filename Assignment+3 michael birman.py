78479d1e6681159c99fed99fedb598a310718d3b
michael-birman <67891272+michael-birman@users.noreply.github.com>
2020-07-22 23:54:21 +0300

    Update Assignment+3 michael birman.py

--- Assignment+3 michael birman.py
+++ Assignment+3 michael birman.py
@@ -312,20 +312,20 @@ answer_nine()
 # In[20]:
 
 
-#def plot9():
-   # import matplotlib as plt
- #   %matplotlib inline
+def plot9():
+    import matplotlib as plt
+    %matplotlib inline
     
- #   Top15 = answer_one()
- #   Top15['PopEst'] = Top15['Energy Supply'] / Top15['Energy Supply per Capita']
- #   Top15['Citable docs per Capita'] = Top15['Citable documents'] / Top15['PopEst']
-  #  Top15.plot(x='Citable docs per Capita', y='Energy Supply per Capita', kind='scatter', xlim=[0, 0.0006])
+    Top15 = answer_one()
+    Top15['PopEst'] = Top15['Energy Supply'] / Top15['Energy Supply per Capita']
+    Top15['Citable docs per Capita'] = Top15['Citable documents'] / Top15['PopEst']
+    Top15.plot(x='Citable docs per Capita', y='Energy Supply per Capita', kind='scatter', xlim=[0, 0.0006])
 
 
-# In[ ]:
+ In[ ]:
 
 
-#plot9() # Be sure to comment out plot9() before submitting the assignment!
+plot9() # Be sure to comment out plot9() before submitting the assignment!
 
 
 # ### Question 10 (6.6%)
@@ -458,25 +458,25 @@ answer_thirteen()
 # In[78]:
 
 
-#def plot_optional():
-  #  import matplotlib as plt
- #   %matplotlib inline
- #   Top15 = answer_one()
- #   ax = Top15.plot(x='Rank', y='% Renewable', kind='scatter', 
-        #            c=['#e41a1c','#377eb8','#e41a1c','#4daf4a','#4daf4a','#377eb8','#4daf4a','#e41a1c',
-     #                  '#4daf4a','#e41a1c','#4daf4a','#4daf4a','#e41a1c','#dede00','#ff7f00'], 
-         #           xticks=range(1,16), s=6*Top15['2014']/10**10, alpha=.75, figsize=[16,6]);
+def plot_optional():
+    import matplotlib as plt
+    %matplotlib inline
+    Top15 = answer_one()
+    ax = Top15.plot(x='Rank', y='% Renewable', kind='scatter', 
+                    c=['#e41a1c','#377eb8','#e41a1c','#4daf4a','#4daf4a','#377eb8','#4daf4a','#e41a1c',
+                       '#4daf4a','#e41a1c','#4daf4a','#4daf4a','#e41a1c','#dede00','#ff7f00'], 
+                   xticks=range(1,16), s=6*Top15['2014']/10**10, alpha=.75, figsize=[16,6]);
 
-  #  for i, txt in enumerate(Top15.index):
-  #      ax.annotate(txt, [Top15['Rank'][i], Top15['% Renewable'][i]], ha='center')
+    for i, txt in enumerate(Top15.index):
+         ax.annotate(txt, [Top15['Rank'][i], Top15['% Renewable'][i]], ha='center')
 
- #   print("This is an example of a visualization that can be created to help understand the data. \
-#This is a bubble chart showing % Renewable vs. Rank. The size of the bubble corresponds to the countries' \
-#2014 GDP, and the color corresponds to the continent.")
-#plot_optional()
+    print("This is an example of a visualization that can be created to help understand the data. \
+This is a bubble chart showing % Renewable vs. Rank. The size of the bubble corresponds to the countries' \
+2014 GDP, and the color corresponds to the continent.")
+plot_optional()
 
 
-# In[ ]:
+ In[ ]:
 
 
 #plot_optional() # Be sure to comment out plot_optional() before submitting the assignment!
